bb34b0ae2852ce7bfa47f2e7a67cf0ef1a443f87
areedv <biorakel@gmail.com>
2015-07-06 10:48:18 +0200

    doc for sample data

---
+++ R/nordummySampleData.R
@@ -0,0 +1,11 @@
+#' Sample dataset for nordummy.
+#'
+#' A dataset containing a sample data with nonesense personal info.
+#' Important for testing purposes. Please provide variable descriptions...
+#'
+#' @name nordummySampleData
+#' @format A data frame with 560 rows and 104 variables:
+#' \describe{
+#' \item{InjuryDt}{Date of injury}
+#' }
+NULL
